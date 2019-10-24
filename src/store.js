e8ba31dcf2d038de12da4fe9c0a21daedd8d5b6d
Nicolas Drouin <ballonposte@gmail.com>
2019-10-24 20:40:11 +0200

    Set for github deploy

--- src/store.js
+++ src/store.js
@@ -5,7 +5,7 @@ import rootReducer from './reducers'
 
 const store = createStore(
   rootReducer,
-  window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+  // window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
 )
 
 export default store
